9b9ebcc057d1d563251a9a8eeee18ff318599aad
NO ONE <sohaibikram249@gmail.com>
2024-07-21 10:18:24 +0500

    update

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const errorMiddleware = require("./middlewares/error-middleware");
 const path = require("path");
 
 const Corsoptions = {
-  origin: "https://solo-tech-theta.vercel.app/",
+  origin: "https://solo-tech-theta.vercel.app",
   methods: ["GET", "POST", "DELETE","PUT","PATCH"],
   credentials: true,
 };
